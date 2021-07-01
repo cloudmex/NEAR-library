d24e8397e069a69f1f2abe1c6ede7fda71a961d9
dokxo96 <dokxo96@gmail.com>
2021-06-30 20:36:52 -0600

    first SM on ASB

---
+++ src/library/__test__/index.unit.spec.ts
@@ -0,0 +1,12 @@
+import * as contract from "../assembly";
+
+describe("Contract", () => {
+    // VIEW method tests
+  
+    it("says hello", () => {
+      expect(contract.helloWorld()).toStrictEqual("hello world")
+    })
+  
+    
+  })
+  
---
+++ src/library/assembly/index.ts
@@ -0,0 +1,7 @@
+import { storage, Context } from "near-sdk-as"
+
+// return the string 'hello world'
+export function helloWorld(): string {
+    return 'hello world'
+  }
+  
