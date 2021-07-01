a1c0e5fdbdca7d74f2ba94b7ec5cf73b782d6947
dokxo96 <dokxo96@gmail.com>
2021-06-30 22:46:45 -0600

    Added at the contract a method to add a new Book

--- src/library/assembly/index.ts
+++ src/library/assembly/index.ts
@@ -1,7 +1,42 @@
 import { storage, Context } from "near-sdk-as"
+import {Books,BookInfomation} from "../models"
 
+const MAX_DESCRIPTION_LENGTH :i32 =255;
+const MAX_BOOKPAGE_LENGTH :u64 =1200;
 // return the string 'hello world'
 export function helloWorld(): string {
     return 'hello world'
   }
-  
+
+/**
+ * 
+ * @param isbn 
+ * @param name 
+ * @param description 
+ * @param numpage 
+ * @param author 
+ * @param datepublished 
+ * @param editions 
+ */
+export function AddBook(
+        isbn:string,
+        name:string,
+        description:string,
+        numpage:u64,
+        author:string,
+        datepublished:string,
+        editions:u64
+):void{
+  assert(isbn.length>0 ,"the ISBN is required")
+  assert(name.length>0,"the name is required")
+  assert(description.length>0  && description.length< MAX_DESCRIPTION_LENGTH,"the description is required or you exceed the character's number")
+  assert(numpage>0 && numpage< MAX_BOOKPAGE_LENGTH,"the numpage is required or you exceed the page's number")
+  assert(author.length>0 ,"the author is required")
+  assert(datepublished.length>0 ,"the datepublished is required")
+  assert(editions >0 ,"the editions is required")
+
+   Books.push(
+     new BookInfomation(Books.length,Context.sender,
+      isbn,name,description,numpage,author,datepublished,editions,Context.blockTimestamp)
+   );
+}
---
+++ src/library/models.ts
@@ -0,0 +1,67 @@
+import { PersistentMap, u128,PersistentVector } from "near-sdk-core";
+
+type AccountId = string;
+type Address = string;
+
+/**
+ * rate th books
+ * 
+ */
+ export enum Rated {
+    bad,
+    regular,
+    awesome,
+    none,
+  }
+
+  @nearBindgen
+  export class BookInfomation{
+    id:u64;
+    owner: AccountId;
+    isbn:string;
+    name:string;
+    description:string;
+    numpage:u64;
+    author:string;
+    datepublished:string;
+    editions:u64;
+    rate:PersistentMap<AccountId,Rated>;
+    comments:PersistentMap<AccountId,string>;
+    timestamp: u64;
+    /**
+     * @param isbn is the international standard book number
+     * @param rate is the book's qualification
+     * @param comments is the list of comments made by the users
+     */
+    constructor(
+        id:u64,
+        owner: AccountId,
+        isbn:string,
+        name:string,
+        description:string,
+        numpage:u64,
+        author:string,
+        datepublished:string,
+        editions:u64,
+        timestamp: u64,
+        ){
+           this.id=id;
+           this.owner=owner;
+           this.isbn=isbn;
+           this.name=name;
+           this.description=description;
+           this.numpage=numpage;
+           this.author=author;
+           this.rate= new PersistentMap<AccountId,Rated>("v");
+           this.rate.set(owner,4);
+           this.comments = new PersistentMap<AccountId,string>("v");
+           this.comments.set(owner,"no comments yet")
+           this.datepublished=datepublished;
+           this.editions=editions; 
+           this.timestamp=timestamp;
+        }
+        
+
+  }
+   
+  export let Books = new PersistentVector<BookInfomation>("Books")
